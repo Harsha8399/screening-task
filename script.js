675466621f1bc46b4dac1c821e8e3d0ae9588883
Harshavardhan Reddy <78035131+Harsha8399@users.noreply.github.com>
2022-10-09 19:41:46 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,31 +3,29 @@ let startBtn = document.getElementById("start");
 let stopBtn = document.getElementById("stop");
 let resetBtn = document.getElementById("reset");
 
-window.onload = function () {
-  var count = 0;
-  var pause = false;
-  console.log(count);
-  var timer;
-  startBtn.addEventListener("click", () => {
-    pause = false;
-    clearInterval(timer);
-    timer = setInterval(() => {
-      {
-        if (pause === false) {
-          count = count + 1;
-          console.log(count);
-          counter.innerHTML = count;
-        }
+var count = 0;
+var pause = false;
+console.log(count);
+var timer;
+startBtn.addEventListener("click", () => {
+  pause = false;
+  clearInterval(timer);
+  timer = setInterval(() => {
+    {
+      if (pause === false) {
+        count = count + 1;
+        console.log(count);
+        counter.innerHTML = count;
       }
-    }, 1000);
-  });
+    }
+  }, 1000);
+});
 
-  resetBtn.addEventListener("click", () => {
-    pause = true;
-    count = 0;
-    counter.innerHTML = " ";
-  });
-  stopBtn.addEventListener("click", () => {
-    pause = true;
-  });
-};
+resetBtn.addEventListener("click", () => {
+  pause = true;
+  count = 0;
+  counter.innerHTML = " ";
+});
+stopBtn.addEventListener("click", () => {
+  pause = true;
+});
