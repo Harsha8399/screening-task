b6db16669bf3c81fd665860d4e48c646b2f97382
Harshavardhan Reddy <78035131+Harsha8399@users.noreply.github.com>
2022-10-09 19:32:02 +0530

    timer task

---
+++ script.js
@@ -0,0 +1,33 @@
+let counter = document.getElementById("counter");
+let startBtn = document.getElementById("start");
+let stopBtn = document.getElementById("stop");
+let resetBtn = document.getElementById("reset");
+
+window.onload = function () {
+  var count = 0;
+  var pause = false;
+  console.log(count);
+  var timer;
+  startBtn.addEventListener("click", () => {
+    pause = false;
+    clearInterval(timer);
+    timer = setInterval(() => {
+      {
+        if (pause === false) {
+          count = count + 1;
+          console.log(count);
+          counter.innerHTML = count;
+        }
+      }
+    }, 1000);
+  });
+
+  resetBtn.addEventListener("click", () => {
+    pause = true;
+    count = 0;
+    counter.innerHTML = " ";
+  });
+  stopBtn.addEventListener("click", () => {
+    pause = true;
+  });
+};
